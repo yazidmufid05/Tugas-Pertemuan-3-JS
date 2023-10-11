cebf5ef149203e8d25bfd346b5fc9b20eb414e3a
yazidmufid05 <144298019+yazidmufid05@users.noreply.github.com>
2023-10-11 16:20:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let nama = prompt("Masukkan angka kamu")
+
+
+let kehadiran = prompt("Masukkan 2 jika benar, masukkan 0 jika salah")
+
+
+let cek = confirm("apakah sudah benar?")
+
+let hasil = kehadiran == 2 ? "benar" : "salah"
+
+
+
+
+alert(hasil)
+
+    
